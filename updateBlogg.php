cf890d0b3d247f4cd2997da47717c776f90e4fc8
Fredric Persson <freper7912@skynet.halmstad.se>
2018-10-05 15:27:46 +0200

    xss protection started

--- updateBlogg.php
+++ updateBlogg.php
@@ -1,23 +1,16 @@
 <?php
  header("Content-type:application/json");
-// Detta är en ARRAY!!!!!!!!!!!!!!!!!!!!!!!!!
-// fast en hybrid mellan objekt och array
-// kallad Associative Array
-//var_dump($_POST);
-
-/* foreach($_POST as $val)
-{
-    echo $val. "<hr>";
-
-} */
-
 
 $oldContent = json_decode( file_get_contents("blogg.json") );
-$oldContent[] = $_POST;
-//var_dump($oldContent);
+
+$formContent = $_POST;
+$formContent['id'] = time();
+
+$oldContent[] = $formContent;
+
 file_put_contents("blogg.json", json_encode($oldContent));
 
-header("Location: blogg.php");
+header("Location: blogg.json");
 
 
 
